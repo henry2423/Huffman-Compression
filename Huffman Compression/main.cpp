a792fc2c469d27124d00efee64f428e83c3d6a28
Henry <henry2423@gmail.com>
2016-12-18 16:03:08 +0800

    Initial Commit

---
+++ Huffman Compression/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Huffman Compression
+//
+//  Created by Henry on 18/12/2016.
+//  Copyright © 2016 henry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
